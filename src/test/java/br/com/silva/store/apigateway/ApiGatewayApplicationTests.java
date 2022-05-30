0b5803f1fd5d21e11a39653829d4e91953e4bbfd
JEFERSON FERREIRA DA SILVA <jeferson.ferreira.ca@gmail.com>
2022-05-30 18:23:16 -0300

    first commit

---
+++ src/test/java/br/com/silva/store/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silva.store.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
